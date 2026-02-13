2782ee2c8753ca9992e7c9dc0aa3b62ba3e81b40
Soju <gattadesmond@gmail.com>
2026-02-13 14:44:14 +0700

    ađ

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,11 +31,11 @@ export default function RootLayout({
             <div className="relative isolate flex min-h-svh flex-col overflow-clip [--header-height:4rem]">
               <div
                 aria-hidden="true"
-                className="before:-left-5 after:-right-5 container pointer-events-none absolute inset-0 z-45 before:absolute before:inset-y-0 before:w-px before:bg-border/64 after:absolute after:inset-y-0 after:w-px after:bg-border/64"
+                className="before:-left-8 after:-right-8 container pointer-events-none absolute inset-0 z-45 before:absolute before:inset-y-0 before:w-px before:bg-border/64 after:absolute after:inset-y-0 after:w-px after:bg-border/64"
               />
               <div
                 aria-hidden="true"
-                className="before:-left-[19px] before:-ml-1 after:-right-[19px] after:-mr-1 container pointer-events-none fixed inset-0 z-45 before:absolute before:top-[calc(var(--header-height)-4.5px)] before:z-1 before:size-2 before:rounded-[2px] before:border before:border-border before:bg-popover before:bg-clip-padding before:shadow-xs/5 after:absolute after:top-[calc(var(--header-height)-4.5px)] after:z-1 after:size-2 after:rounded-[2px] after:border after:border-border after:bg-background after:bg-clip-padding after:shadow-xs/5"
+                className="before:-left-[32px] before:-ml-1 after:-right-[32px] after:-mr-1 container pointer-events-none fixed inset-0 z-45 before:absolute before:top-[calc(var(--header-height)-4.5px)] before:z-1 before:size-2 before:rounded-[2px] before:border before:border-border before:bg-popover before:bg-clip-padding before:shadow-xs/5 after:absolute after:top-[calc(var(--header-height)-4.5px)] after:z-1 after:size-2 after:rounded-[2px] after:border after:border-border after:bg-background after:bg-clip-padding after:shadow-xs/5"
               />
               <SiteHeader />
               {children}
